8534de621fed709a19d39b4f74b6e6e5c36b1ed9
danwalk <danielwalker87@hotmail.com>
2021-05-24 00:16:18 +0200

    titanic

--- walk_func.py
+++ walk_func.py
@@ -11,6 +11,29 @@ def dice_roller(num1=1, num2=7):
     g = random.randint(num1, num2)
     return g
 
+# for webscraping, the url sometimes has a date at the end, that each day changes for up to date scores e.g https://masseyratings.com/nba/games?dt=20201222
+def dateurlchanger(url, adddays, maxdate):
+    dateofpage = url[-8:]
+    date_time_obj = datetime.strptime(dateofpage, '%Y%m%d')
+    maxd = datetime.strptime(maxdate, '%Y%m%d')
+    if date_time_obj >= maxd:
+        return 'UP TO DATE'
+    else:
+        date_time_obj1 = date_time_obj + timedelta(days=adddays) 
+        date_time_obj1 = date_time_obj1.strftime('%Y%m%d')
+        return url.replace(dateofpage, date_time_obj1)
+
+#for webscraping, the url sometimes has a page number at the end  e.g https://www.oddsportal.com/basketball/usa/nba/results/#/page/1
+def pageurlchanger(url, adddays, maxpage): #works up to 9
+    page = int(url[-1:])
+    if page >= maxpage:
+        return 'UP TO DATE'
+    else:
+        return url.replace(url[-1:], str(page+adddays))
+
+def pageadderurl(url, counter):
+    return url + str(counter)
+
 #pd.options.display.float_format = "{:,.2%}".format ------ modifiy column format, decimal places
 
 
@@ -26,3 +49,7 @@ def dice_roller(num1=1, num2=7):
 # pd.read_csv('tips.csv', index_col=0) ------ import and use first column as index
 
 # pd.read_csv("data\\cars1.csv") ------ use \\ to move backwards and forwards in file path
+
+# https://www.opentechguides.com/how-to/article/pandas/193/index-slice-subset.html slicing help
+
+#https://www.marsja.se/six-ways-to-reverse-pandas-dataframe/
